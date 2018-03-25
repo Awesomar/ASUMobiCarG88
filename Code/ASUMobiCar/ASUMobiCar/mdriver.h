58e5566124c4312e173faf6640d8cbbd0a369662
Ammar Yasser <ammaryasser085@gmail.com>
2018-03-26 00:58:36 +0200

    make motor driver library

---
+++ Code/ASUMobiCar/ASUMobiCar/mdriver.h
@@ -0,0 +1,25 @@
+﻿/*
+ * mdriver.h
+ *
+ * Created: 25/03/18 
+ *  Author: Ammar 
+ */ 
+
+
+#ifndef DRIVER_H_
+#define DRIVER_H_
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+
+#define DDR_MOTOR DDRB
+#define PORT_MOTOR PORTB
+
+void Motor_Init(int8_t IN1, int8_t IN2, int8_t IN3, int8_t IN4);
+void moveForward();
+void moveBackward();
+void moveRight();
+void moveLeft();
+void stop();
+
+#endif /* DRIVER_H_ */
