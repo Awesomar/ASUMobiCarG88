2cef8bc79c748e58477b0ce905f1a2b744e6c092
Ammar Yasser <ammaryasser085@gmail.com>
2018-03-26 00:59:38 +0200

    macros file

---
+++ Code/ASUMobiCar/ASUMobiCar/macros.h
@@ -0,0 +1,24 @@
+﻿/*
+ * macros.h
+ *
+ * The avr/macros.h file provides a set of macros
+ * necessary for compiling AVR. These are provided
+ * to offer analogous functionality on Windows.
+ *
+ * Created: 26/03/18 
+ *  Author: Ammar
+ */ 
+
+
+#ifndef MACROS_H_
+#define MACROS_H_
+
+#include  <avr/io.h>
+
+#define BIT_SET(ADDRESS,BIT) (ADDRESS |= (1<<BIT))
+#define BIT_CLEAR(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT))
+#define BIT_CHECK(ADDRESS,BIT) (ADDRESS & (1<<BIT))
+#define BIT_FLIP(ADDRESS,BIT) (ADDRESS ^= (1<<BIT))
+
+
+#endif /* MACROS_H_ */
