eaaee8fcd3dd27ed6f5022bf9e1c8d418cb12084
Aya Hany <ayah17897@gmail.com>
2018-03-28 21:48:05 +0200

    add Bluetooth code to arduino code

--- Code/Arduino Code/car/car.ino
+++ Code/Arduino Code/car/car.ino
@@ -5,6 +5,7 @@ const int motorB2 = 4;
 int state = '0';
 
 void setup() {
+  Serial.begin(9600);
 pinMode(motorA1, OUTPUT);
 pinMode(motorA2, OUTPUT);
 pinMode(motorB1, OUTPUT);
@@ -13,31 +14,39 @@ pinMode(motorB2, OUTPUT);
 }
 
 void loop() {
-  if(state=='f'){ // forward
+  if(Serial.available()>0){
+state = Serial.read();
+    }
+    if(state=='f'){// forward
+      Serial.println(state);
 digitalWrite(motorA1, HIGH); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, HIGH); 
 digitalWrite(motorB2, LOW);
   }
 if(state=='b'){ // Backward
+  Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, HIGH);
 digitalWrite(motorB1, LOW); 
 digitalWrite(motorB2, HIGH); 
 }
 if(state=='r'){ // Right
+  Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, HIGH); 
 digitalWrite(motorB2, LOW); 
 }
 if(state=='l'){ // Left
+  Serial.println(state);
 digitalWrite(motorA1, HIGH); 
 digitalWrite(motorA2, LOW);
 digitalWrite(motorB1, LOW);
 digitalWrite(motorB2, LOW); 
 } 
 if(state=='s'){ // Stop
+  Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, LOW); 
