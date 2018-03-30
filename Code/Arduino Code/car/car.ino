767a5a4205196b2e00c225a29208dd953fecca67
3omar-a7maad <36338989+3omar-a7maad@users.noreply.github.com>
2018-03-30 19:31:35 +0200

    Update car.ino
    *a little bit of reorganization
    *adding the ultrasonic sensor
    *adding the easy drive mode

--- Code/Arduino Code/car/car.ino
+++ Code/Arduino Code/car/car.ino
@@ -1,8 +1,22 @@
+void Forward () ;
+void Backward () ;
+void Right () ;
+void Left () ; 
+void Stop () ;
+void EasyDrive () ;
+float ReadPing ();
 const int motorA1 = 7;
 const int motorA2 = 6;
 const int motorB1 = 5;
 const int motorB2 = 4;
-int state = '0';
+char state ;
+float distance ;
+#define trig A0
+#define echo A1
+#include<NewPing.h> 
+NewPing sonar(trig, echo, 250)
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -14,42 +28,85 @@ pinMode(motorB2, OUTPUT);
 }
 
 void loop() {
+  distance = ReadPing
+    if (distance<=15){Stop ();}
   if(Serial.available()>0){
 state = Serial.read();
     }
     if(state=='f'){// forward
-      Serial.println(state);
+      Forward ();
+  }
+if(state=='b'){ // Backward
+   
+Backward () ;
+}
+if(state=='r'){ // Right
+  
+Right () ;
+}
+if(state=='l'){ // Left
+  
+Left () ;
+} 
+if(state=='s'){ // Stop
+Stop () ;  
+}
+  if(state=='e'){ // EasyDrive
+    EasyDrive () ;
+}
+}
+void Forward ()
+{
+  Serial.println(state);
 digitalWrite(motorA1, HIGH); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, HIGH); 
 digitalWrite(motorB2, LOW);
-  }
-if(state=='b'){ // Backward
+}
+void Backward ()
+{
   Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, HIGH);
 digitalWrite(motorB1, LOW); 
-digitalWrite(motorB2, HIGH); 
+digitalWrite(motorB2, HIGH);
 }
-if(state=='r'){ // Right
+
+void Right ()
+{
   Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, HIGH); 
 digitalWrite(motorB2, LOW); 
 }
-if(state=='l'){ // Left
+void Left () 
+{
   Serial.println(state);
 digitalWrite(motorA1, HIGH); 
 digitalWrite(motorA2, LOW);
 digitalWrite(motorB1, LOW);
 digitalWrite(motorB2, LOW); 
-} 
-if(state=='s'){ // Stop
+}
+void Stop () 
+{
   Serial.println(state);
 digitalWrite(motorA1, LOW); 
 digitalWrite(motorA2, LOW); 
 digitalWrite(motorB1, LOW); 
 digitalWrite(motorB2, LOW);
 }
+void EasyDrive ()
+{
+  Forward () ;
+  if (distance <=15)
+  {Stop();}
+}
+float ReadPing ()
+{
+  delay(70);
+float cm= sonar.ping_cm();
+if (cm==0)
+  cm = 250;
+return cm;
 }
