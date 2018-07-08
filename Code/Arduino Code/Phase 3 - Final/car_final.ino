cd5c81b35464b8797901f18708e795933724c7f0
Hafez37 <36421815+Hafez37@users.noreply.github.com>
2018-07-08 17:17:18 +0200

    Update car_final.ino
    ِdded function prototypes and variables

--- Code/Arduino Code/Phase 3 - Final/car_final.ino
+++ Code/Arduino Code/Phase 3 - Final/car_final.ino
@@ -1,3 +1,30 @@
+#include <NewPing.h>
+
+// Bluetooth MAC address
+// 00:21:13:00:E0:D7 
+
+void Forward(), Backward(), Right(), Left(), Stop(), Rotate(int c), EasyDrive(), Follower(), 
+     Distance(int number), Angle(int angle), Square(), Circle(bool dir, int x, int y, int z), Infinity(), Demo(), Phase1(), Phase2(), Phase3(), Left2(), Right2();
+int ReadPing ();
+int max_counts; 
+const int motorA1 = 7;
+const int motorA2 = 6;
+const int motorB1 = 5;
+const int motorB2 = 4;
+const int enA = 10;
+const int enB = 11;
+int velocity = 100;
+int counter = 0;
+char state = 0;
+int dist, number;
+int MS,LS,RS,MSV,LSV,RSV;
+#define trig A0
+#define echo A1
+int distance;
+char s= 's';
+
+
+NewPing sonar(trig, echo, 250);
 void Phase1(), Phase2(), Phase3();
 
 
