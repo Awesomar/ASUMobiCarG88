9beafeb99fcc2d2a303d48ffc47aaadef8c8b3e9
Donia Anis <36480523+Doniaanis@users.noreply.github.com>
2018-07-08 17:07:26 +0200

    added Phase 1 Function

--- Code/Arduino Code/Phase 3 - Final/car_final.ino
+++ Code/Arduino Code/Phase 3 - Final/car_final.ino
@@ -34,3 +34,68 @@ void loop()
       default: Stop();
     }
 }
+
+void Phase1()
+{
+  Serial.println("Phase1");
+  int numb;
+  while(true)
+  {
+    if (Serial.available() > 0)
+      state = Serial.read();
+  /*  
+    while (Serial.available() == 0);
+    state = Serial.read();
+    numb = Serial.parseInt();
+   */
+    switch (state)
+    {
+      //case 'R': Rotate(numb); break;
+        
+      case 'f' :
+          Forward();
+          Serial.println("Forward()");
+          //delay(10);
+          //state = 0;
+          break;
+          
+      case 'b' :
+          Backward();
+          Serial.println("Backward()");
+          //delay(10);
+          //state = 0;
+          break;
+          
+      case 'l' :
+          Left();
+          Serial.println("Left()");
+          //delay(10);
+          //state = 0;
+          break;
+        
+      case 'r' :
+          Right();
+          Serial.println("Right()");
+          //delay(10);
+          //state = 0;
+          break;
+
+      case 's' : Stop(); break;
+
+      case 'e' : EasyDrive(); break;
+  
+          
+      case 'B' :
+          Phase2();
+          state = 0;
+          break;
+          
+       case 'C' :
+          Phase3();
+          state = 0;
+          break;
+        
+       default: Stop();
+      }
+  }
+}
